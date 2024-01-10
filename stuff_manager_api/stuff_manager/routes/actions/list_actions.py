4445bacdaed2dd1eed29a67357b97d69e6b7a6de
brendonk13 <brendonk13.bk@gmail.com>
2024-01-10 15:03:45 -0500

    cleanup non-working async code from list actions queryset

--- stuff_manager_api/stuff_manager/routes/actions/list_actions.py
+++ stuff_manager_api/stuff_manager/routes/actions/list_actions.py
@@ -32,61 +32,20 @@ ListActionsResponseSchema = list[Optional[ActionDBSchema]]
 # todo: cannot paginate async yet
 # https://github.com/vitalik/django-ninja/pull/1030/commits
 # @paginate
-def list_actions(request, filters: Query[ActionQueryFilterSchema]):
+def list_actions(request, query_filters: Query[ActionQueryFilterSchema]):
     user = request.auth[0]
-    # data = [
-    #     action_tag
-    #     async for action_tag
-    #     # in filters.filter(Actions_Tags.objects.filter(action__user_id=user.id).select_related("action", "tag")).distinct()
-    #     # in filters.filter(Actions_Tags.objects.filter(action__user_id=user.id).select_related("action")).distinct()
-    #     in filters.filter(Action.objects.filter(user_id=user.id).prefetch_related("actions_tags_set")).distinct()
-    # ]
-
     data = []
-    # async for action in filters.filter(Action.objects.filter(user_id=user.id).prefetch_related("actions_tags_set", "actions_requiredcontexts_set")).distinct():
-    # async for action in filters.filter(Action.objects.filter(user_id=user.id).prefetch_related("actions_tags_set", "actions_requiredcontexts_set")).distinct():
-    # async for action in filters.filter(Action.objects.filter(user_id=user.id)).distinct().aiterator():
-    for action in filters.filter(Action.objects.filter(user_id=user.id).prefetch_related("actions_tags_set", "actions_requiredcontexts_set")).distinct():
-        # print("type", type(action))
-        # action.tags = [action.tag.value async for action in action.actions_tags_set.all()]
-        # action.tags = action.actions_tags_set.only("tag__value").all()
-
-        # tags = [i.tag_id for i in action.actions_tags_set.all()]
-        # print("tags", tags)
-
-        # action.tags = sync_to_async.actions_tags_set.all()
-        # action.tags = []
-        # for tag in action.actions_tags_set.all():
-            # print("tag", tag)
-            # action.tags.append(tag.tag.value)
-        used = {
+    for action in query_filters.filter(Action.objects.filter(user_id=user.id).prefetch_related("actions_tags_set", "actions_requiredcontexts_set")).distinct():
+        formatted_data = {
             "id": action.id,
             "title": action.title,
             "description": action.description,
             "energy": action.energy,
             "project_id": action.project_id,
             "created": action.created,
-            # "tags": tags,
-            # "tags": [thing.tag async for thing in await sync_to_async(action.actions_tags_set.only("tag__value").all)()],
-            # "tags": [thing async for thing in await sync_to_async(action.actions_tags_set.all)()],
-            # "tags": [thing.tag async for thing in action.actions_tags_set.all()],
-            # "tags": [thing.tag async for thing in action.actions_tags_set.all()],
             "tags": [thing.tag.value for thing in action.actions_tags_set.all()],
             "required_context": [thing.tag.value for thing in action.actions_requiredcontexts_set.all()],
         }
-        data.append(used)
+        data.append(formatted_data)
 
-
-    # print("data", data)
     return data
-
-
-# async def list_action_tags(queryset, user_id: int, filters: Query[Actions_TagsQueryFilterSchema]):
-#     return [
-#         action_tag.action
-#         async for action_tag
-#         in filters.filter(queryset.filter(action__user_id=user_id).select_related("action")).distinct()
-#         # in filters.filter(queryset.filter(action__user_id=user_id).select_related("action", "tag")).distinct()
-#     ]
-
-
--- stuff_manager_api/stuff_manager/schemas/action.py
+++ stuff_manager_api/stuff_manager/schemas/action.py
@@ -12,36 +12,11 @@ class ActionQueryFilterSchema(FilterSchema):
     energy           : Optional[int]       = None
     project_id       : Optional[int]       = None
 
-#     def custom_expression(self) -> Q:
-#         q = Q()
-#         if self.title:
-#             q &= Q(title=self.title)
-#         if self.energy:
-#             q &= Q(energy=self.energy)
-#         if self.date: # todo: fix this to work with dates and not datetimes
-#             q &= Q(date=self.date)
-#         # parse query string
-
-#         # todo: what happens if a context and a tag have the same name ????
-#         # tags = []
-#         if self.tags:
-#             tags = [tag.strip("\"'") for tag in self.tags[0].rstrip("]").lstrip("[").split(",")]
-#             q &= Q(tag__value__in=tags)
-#         if self.required_context:
-#             tags = [tag.strip("\"'") for tag in self.required_context[0].rstrip("]").lstrip("[").split(",")]
-#             q &= Q(tag__value__in=tags)
-
-#         # if self.tags or self.required_context:
-#             # q &= Q(tag__value__in=tags)
-#         print(q)
-#         return q
-
     def filter_tags(self, _tags: Optional[list[str]]) -> Q:
         if not _tags:
             return Q()
         # parse query string
         tags = [tag.strip("\"'") for tag in _tags[0].rstrip("]").lstrip("[").split(",")]
-        # return Q(tag__value__in=tags)
         return Q(actions_tags__tag__value__in=tags)
 
     def filter_required_context(self, _tags: Optional[list[str]]) -> Q:
@@ -50,5 +25,4 @@ class ActionQueryFilterSchema(FilterSchema):
             return Q()
         # parse query string
         tags = [tag.strip("\"'") for tag in _tags[0].rstrip("]").lstrip("[").split(",")]
-        # return Q(tag__value__in=tags)
         return Q(actions_requiredcontexts__tag__value__in=tags)
